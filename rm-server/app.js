e497d5734ebdd3e9d20d28c7ffc89d5b82457488
Jigar Solanki <jigar234@gmail.com>
2015-11-22 00:29:59 -0500

    Add device support for testing

--- rm-server/app.js
+++ rm-server/app.js
@@ -3,24 +3,64 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var deviceSocket;
+var panelSocket;
+
 app.use('/static', express.static('public'));
 
 app.get('/', function (req, res){
   res.sendfile('index.html');
 });
 
+app.get('/device', function (req, res){
+  res.sendfile('device.html');
+});
+
 io.on('connection', function (socket) {
-  console.log('a user connected');
+  console.log('New connetion');
 
   socket.on('disconnect', function () {
-    console.log('user disconnected');
+    if (socket === panelSocket) {
+      panelSocket = null;
+
+      if (deviceSocket) {
+        deviceSocket.emit('panel_disconnected');
+      }
+
+      console.log('Panel disconnected');
+    } else if (socket === deviceSocket) {
+      deviceSocket = null;
+
+      if (panelSocket) {
+        panelSocket.emit('device_disconnected');
+      }
+
+      console.log('Device disconnected');
+    }
   });
 
-  socket.on('move', function (msg) {
-    console.log('MOVE: ' + msg);
-  });
-  socket.on('stop', function () {
-    console.log('STOP');
+  socket.on('register', function (msg) {
+    if (msg === 'panel' && !panelSocket) {
+      panelSocket = socket;
+      console.log('Panel connected');
+    } else if (msg === 'device' && !deviceSocket) {
+      deviceSocket = socket;
+      console.log('Device connected');
+    }
+
+    if (panelSocket && deviceSocket) {
+      panelSocket.on('move', function (msg) {
+        deviceSocket.emit('move', msg);
+        console.log('MOVE: ' + msg);
+      });
+      panelSocket.on('stop', function () {
+        deviceSocket.emit('stop');
+        console.log('STOP');
+      });
+
+      panelSocket.emit('ready');
+      deviceSocket.emit('ready');
+    }
   });
 });
 
