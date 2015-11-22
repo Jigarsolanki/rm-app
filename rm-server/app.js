3229dc8dd7ad2e019eca5d65b22368eacb13f4e8
Jigar Solanki <jigar234@gmail.com>
2015-11-21 23:44:48 -0500

    Add basic functions

---
+++ rm-server/app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use('/static', express.static('public'));
+
+app.get('/', function (req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+
+  socket.on('move', function (msg) {
+    console.log('MOVE: ' + msg);
+  });
+  socket.on('stop', function () {
+    console.log('STOP');
+  });
+});
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+});
